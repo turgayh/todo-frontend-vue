32d4acf11534d9622e194a0f47d2f9021f7a235d
Hakan Turgay <turgayh@itu.edu.tr>
2021-04-15 23:54:31 +0300

    create component for todolist

---
+++ src/components/TodoList.vue
@@ -0,0 +1,14 @@
+<template>
+    <div class = 'container'>
+    <h1>Todo App</h1>    
+    </div>
+</template>
+
+<script>
+export default {
+  name: "TodoList",
+};
+</script>
+
+<style >
+</style>
