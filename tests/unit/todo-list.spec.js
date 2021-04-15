ba67e0d1f7abaff4a73741d4610d35a72dd09eec
Hakan Turgay <turgayh@itu.edu.tr>
2021-04-16 00:01:45 +0300

    remove HelloWorld & Added unit test TodoList 

--- tests/unit/todo-list.spec.js
+++ tests/unit/todo-list.spec.js
@@ -1,12 +1,12 @@
 import { shallowMount } from "@vue/test-utils";
-import HelloWorld from "@/components/HelloWorld.vue";
+import TodoList from "@/components/TodoList.vue";
 
 describe("HelloWorld.vue", () => {
   it("renders props.msg when passed", () => {
     const msg = "new message";
-    const wrapper = shallowMount(HelloWorld, {
+    const wrapper = shallowMount(TodoList, {
       props: { msg },
     });
-    expect(wrapper.text()).toMatch(msg);
+    expect(wrapper.text()).toMatch("Todo");
   });
 });
