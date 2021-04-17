ed20a209687e39959228fffcc9cf8bafa220d0b1
Hakan Turgay <turgayh@itu.edu.tr>
2021-04-17 22:40:17 +0300

    added e2e  test

--- tests/e2e/specs/test.js
+++ tests/e2e/specs/test.js
@@ -1,8 +1,13 @@
 // https://docs.cypress.io/api/introduction/api.html
 
 describe("My First Test", () => {
-  it("Visits the app root url", () => {
+  it("should added task to list", () => {
     cy.visit("/");
-    cy.contains("h1", "Welcome to Your Vue.js App");
+    const uuid = () => Cypress._.random(0, 1e6);
+    const data = uuid();
+    cy.get("input").type(data);
+    cy.get("button").click();
+    cy.wait(3000);
+    cy.get(".todo-item").contains(data);
   });
 });
